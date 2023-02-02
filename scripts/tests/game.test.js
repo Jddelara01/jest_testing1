36f01af17869c996658a02609380fbe9bbd64c06
Jddelara01 <jddelara01@gmail.com>
2023-02-02 11:44:22 +0000

    Add newGame function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,4 +1,8 @@
-const { game } = require("../game");
+/**
+ * @jest-environment jsdom
+ */
+
+const { game, newGame, showScore } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -23,5 +27,27 @@ describe("game object contains correct keys", () => {
     });
     test("choices contains the correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerHTML = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should clear current game array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should display zero for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
